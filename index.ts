d82696f7d94fb7e2cd82e14813dab5ae77bb6744
Varunan_V <47948397+varunan-vara@users.noreply.github.com>
2021-06-06 13:52:29 -0400

    Began 'npm watch', Got basic 'Ping Pong' bot working

--- index.ts
+++ index.ts
@@ -1,3 +1,19 @@
 //Imports
-//Instantiate Client/Bot
-//Main Code
+import * as dotenv from "dotenv";
+dotenv.config();
+import {Client, Message} from "discord.js";
+
+//Consts
+const client = new Client();
+
+client.on("ready", () => {
+    console.log("I'm Ready!");
+})
+client.on("message", (msg:Message) => {
+    if (msg.content === "ping") {
+        msg.reply("pong");
+    }
+})
+
+client.login(process.env.TOKEN);
+
